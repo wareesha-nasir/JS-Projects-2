cc55ed070d52ad951d80bdc790f4378acae07b2c
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-06-25 17:30:01 +0000

    Add files via upload

--- hex color code/play.js
+++ hex color code/play.js
@@ -2,3 +2,14 @@ const hexcolor=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 const btn=document.querySelector('.click-btn');
 let body=document.querySelector('body');
 let hex=document.querySelector('.hex');
+btn.addEventListener('click',hexCode);
+
+function hexCode(){
+    let a='#';
+    for(let i=0;i<6;i++){
+        let ran=Math.floor(Math.random()*hexcolor.length)
+        a+=hexcolor[ran]
+    }
+    body.style.backgroundColor=a;
+    hex.innerHTML=a;
+}
