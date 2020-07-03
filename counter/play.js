b2079a838c0fd8b496bf407fc76589ca3d875ef6
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-07-03 18:51:35 +0000

    Add files via upload

---
+++ counter/play.js
@@ -0,0 +1,8 @@
+const add=document.querySelector(".addcounter");
+const lower=document.querySelector('.lowercounter');
+const count=document.querySelector('.counter');
+
+
+
+add.addEventListener('click',addCounter);
+lower.addEventListener('click',lowerCounter)
