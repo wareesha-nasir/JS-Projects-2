9c51685a2882285c64a2a15a1fdc7855614d3ca6
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-07-04 19:02:22 +0000

    Add files via upload

--- counter/play.js
+++ counter/play.js
@@ -1,8 +1,32 @@
-const add=document.querySelector(".addcounter");
-const lower=document.querySelector('.lowercounter');
+const add=document.querySelector("#addcounter");
+const lower=document.querySelector('#lowercounter');
 const count=document.querySelector('.counter');
 
-
+let counter=0;
 
 add.addEventListener('click',addCounter);
-lower.addEventListener('click',lowerCounter)
+lower.addEventListener('click',lowerCounter);
+
+
+function addCounter(){
+    counter++;
+    count.innerHTML=counter;
+    if(count.innerHTML>'0'){
+        count.style.color='green'
+    }
+    else if(count.innerHTML==='0'){
+        count.style.color='white';
+    }
+}
+
+
+function lowerCounter(){
+    counter--;
+    count.innerHTML=counter;
+    if(count.innerHTML<'0'){
+        count.style.color='red' 
+    }
+    else if(count.innerHTML==='0'){
+        count.style.color='white'
+    }
+}
