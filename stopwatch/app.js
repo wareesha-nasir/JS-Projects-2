d1e4f4ccf61e2fddb0766e9c4adbbd3d7d715bc9
wareesha-nasir <57236937+wareesha-nasir@users.noreply.github.com>
2020-07-11 11:59:25 +0000

    Add files via upload

---
+++ stopwatch/app.js
@@ -0,0 +1,44 @@
+var min = document.getElementById("min");
+var sec=document.getElementById("sec");
+var mili=document.getElementById("milli");
+var s=0;
+var m=0;
+var ml=0;
+var interval;
+var start=document.querySelector(".start");
+var st=document.querySelector(".stop");
+var reset=document.querySelector(".reset");
+start.addEventListener("click",startTimer)
+st.addEventListener("click",stopTimer)
+reset.addEventListener("click",resetTimer)
+function timer(){
+    ml++;
+    mili.innerHTML=ml;
+    if(ml>=100){
+        s++;
+        sec.innerHTML=s;
+        ml=0;
+    }
+    if(s>=60){
+        m++;
+        min.innerHTML=m;
+        s=0;
+    }
+}
+
+function startTimer(){
+    interval=setInterval(timer,10)
+    start.disabled=true;
+}
+
+function stopTimer(){
+clearInterval(interval)
+}
+
+function resetTimer(){
+    mili.innerHTML= "0";
+    sec.innerHTML="0";
+    min.innerHTML="0";
+    stopTimer();
+    start.disabled=false
+}
